10dfbd17f10da0bbdf815156055f56fd2b4da42b
1320516705@qq.com <1320516705@qq.com>
2023-09-19 19:41:41 +0800

    master commit3

--- src/main/java/com.atguigu.git/TestGit.java
+++ src/main/java/com.atguigu.git/TestGit.java
@@ -8,7 +8,7 @@ public class TestGit {
         System.out.println("hello git3!");
         System.out.println("hello git4!");
         System.out.println("hot-fix test");
-
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
